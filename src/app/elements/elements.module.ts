2c73c51d07c5137753edb17c5c0516bc986fb7bf
pyavchik <pyavchik@gmail.com>
2020-02-08 18:24:08 +0200

    implemented titled divider

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ElementsRoutingModule } from './elements-routing.module';
-import { ElementsHomeComponent } from './elements-home/elements-home.component';
-import { PlaceholderComponent } from './placeholder/placeholder.component';
-import { TimesDirective } from './times.directive';
+import {ElementsRoutingModule} from './elements-routing.module';
+import {ElementsHomeComponent} from './elements-home/elements-home.component';
+import {PlaceholderComponent} from './placeholder/placeholder.component';
+import {TimesDirective} from './times.directive';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ],
   exports: []
 })
-export class ElementsModule { }
+export class ElementsModule {
+}
