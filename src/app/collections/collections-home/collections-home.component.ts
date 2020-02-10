e0e3307e68f3f6d61b8effd90b9265b2babd611d
pyavchik <pyavchik@gmail.com>
2020-02-10 18:53:41 +0200

    added table data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
-
+  data = [
+    { name: 'James', age: 24, job: 'Designer'},
+    { name: 'Jill', age: 26, job: 'Engineer' },
+    { name: 'Elise', age: 25, job: 'Engineer'}
+  ];
+  headers = [
+    {key: 'name', labeld: 'Name'},
+    {key: 'age', labeld: 'Age'},
+    {key: 'job', labeld: 'Job'}
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CollectionsRoutingModule } from './collections-routing.module';
-import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import {CollectionsRoutingModule} from './collections-routing.module';
+import {CollectionsHomeComponent} from './collections-home/collections-home.component';
+import {TableComponent} from './table/table.component';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [CollectionsHomeComponent],
+  declarations: [CollectionsHomeComponent, TableComponent],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ],
   exports: [
     CollectionsHomeComponent
   ]
 })
-export class CollectionsModule { }
+export class CollectionsModule {
+}
