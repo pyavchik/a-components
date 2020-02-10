86018a8e355246c922d1a829b5b97095565aea0d
pyavchik <pyavchik@gmail.com>
2020-02-10 21:21:26 +0200

    update table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
   data = [
-    { name: 'James', age: 24, job: 'Designer'},
-    { name: 'Jill', age: 26, job: 'Engineer' },
-    { name: 'Elise', age: 25, job: 'Engineer'}
+    { name: 'James', age: 24, job: 'Designer', employed: true},
+    { name: 'Jill', age: 26, job: 'Engineer', employed: false },
+    { name: 'Elise', age: 25, job: 'Engineer', employed: true}
   ];
   headers = [
+    {key: 'employed', label: 'Has a Job?'},
     {key: 'name', label: 'Name'},
     {key: 'age', label: 'Age'},
     {key: 'job', label: 'Job'}
--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +6,13 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  @Input('class') classNames = '';
+
   @Input() data = [];
   @Input() headers = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
